8c300b8188581ca23e1ace3d71c6bef7856e16ed
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-02-29 11:26:25 -0800

    added inquirer.prompt, user may pick from list of things to do

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ connection.connect(function(err) {
     if(err) throw err;
     console.log(`Connected as id ${connection.threadId}`)
 })
+
+inquirer.prompt(
+    [
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'task',
+            choices: ['View all employees', 'View all departments', 'View all roles', 'Add a department', 'Add an employee', 'Add a role']
+        }
+    ]
+).then(function(response) {
+    switch(response.task) {
+        case 'View all employees':
+            console.log('view all');
+        break;
+        case 'View all departments':
+            console.log('view departments');
+        break;
+        case 'View all roles':
+
+        break;
+        case 'Add a department':
+
+        break;
+        case 'Add an employee':
+        
+        break;
+        case 'Add a role':
+        
+        break;
+    }
+})
